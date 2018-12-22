0a096382d7819fd0678b4638247928d4326afbf9
Paul <develop.iwork@gmail.com>
2018-12-22 16:34:05 +0700

    Updated Application.java

--- src/main/java/com/company/springtest/Application.java
+++ src/main/java/com/company/springtest/Application.java
@@ -1,16 +1,10 @@
 package com.company.springtest;
 
-import com.company.springtest.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    private UserService userService;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
