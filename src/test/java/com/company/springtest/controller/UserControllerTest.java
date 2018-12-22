1bcf7fc24d6eaf96534e71d9daba66ff24748e31
Paul <develop.iwork@gmail.com>
2018-12-22 17:14:09 +0700

    Updated UserControllerTest, added application.properties

---
+++ src/test/java/com/company/springtest/controller/UserControllerTest.java
@@ -0,0 +1,78 @@
+package com.company.springtest.controller;
+
+import com.company.springtest.model.User;
+import com.company.springtest.service.UserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private User user1;
+    private User user2;
+
+    @Before
+    public void setUp() {
+        user1 = new User("Alex", 23, null);
+        user2 = new User("Paul", 13, null);
+
+        List<User> userList = Arrays.asList(user1, user2);
+
+        given(userService.findAll()).willReturn(userList);
+        given(userService.findById(user1.getUserId())).willReturn(Optional.of(user1));
+    }
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertNotNull(mockMvc);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        mockMvc.perform(get("/user")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void findById() throws Exception {
+        mockMvc.perform(get("/user/" + user1.getUserId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId", is(user1.getUserId())));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
