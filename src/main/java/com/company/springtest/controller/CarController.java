9e1176dc14e62b07a8d33ba371517f584f527b78
Paul <develop.iwork@gmail.com>
2018-12-22 18:22:22 +0700

    Updated CarController.java and service

--- src/main/java/com/company/springtest/controller/CarController.java
+++ src/main/java/com/company/springtest/controller/CarController.java
@@ -3,11 +3,13 @@ package com.company.springtest.controller;
 import com.company.springtest.model.Car;
 import com.company.springtest.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 public class CarController {
@@ -16,12 +18,12 @@ public class CarController {
     private CarService carService;
 
     @RequestMapping(path = "/car")
-    public Iterable<Car> getAll() {
-        return carService.findAll();
+    public ResponseEntity<List<Car>> getAll() {
+        return new ResponseEntity<>(carService.findAll(), HttpStatus.OK);
     }
 
     @RequestMapping(path = "/car/{id}")
-    public Optional<Car> findById(@PathVariable(name = "id") Integer id) {
-        return carService.findById(id);
+    public ResponseEntity<Car> findById(@PathVariable(name = "id") Integer id) {
+        return new ResponseEntity<>(carService.findById(id).orElse(null), HttpStatus.OK);
     }
 }
--- src/main/java/com/company/springtest/service/CarService.java
+++ src/main/java/com/company/springtest/service/CarService.java
@@ -5,6 +5,7 @@ import com.company.springtest.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,7 +14,7 @@ public class CarService {
     @Autowired
     private CarRepository carRepository;
 
-    public Iterable<Car> findAll() {
+    public List<Car> findAll() {
         return carRepository.findAll();
     }
 
